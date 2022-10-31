ba6ac597e0df405adcb5fd6ebb2b00bb711a1b86
Riccardo Pirruccio <rickp1795@gmail.com>
2022-10-31 08:00:26 -0500

    ssh tunnel to db server established on host server, etl to db server done, ready to deploy

--- server/databases/db.js
+++ server/databases/db.js
@@ -1,6 +1,8 @@
 const { MONGODB_URI } = require('../../config');
 const mongoose = require('mongoose');
 
+// console.log('MONGODB_URI=: ', MONGODB_URI);
+
 module.exports = () => {
   mongoose
     .connect(MONGODB_URI || `mongodb://localhost:27017/ReviewsDB`)
