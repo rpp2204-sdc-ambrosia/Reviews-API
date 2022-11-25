31d1a0684716265105ca2cc84d8278547dc048d1
Riccardo Pirruccio <rickp1795@gmail.com>
2022-11-25 12:17:40 -0600

    added mongoose connection options for performance improvement

--- server/databases/db.js
+++ server/databases/db.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
   mongoose
-    .connect(MONGODB_URI || `mongodb://localhost:27017/ReviewsDB`)
+    .connect(MONGODB_URI || `mongodb://localhost:27017/ReviewsDB`, {
+      useUnifiedTopology: true,
+      family: 4,
+    })
     .then(() => console.log('db connection success'))
     .catch((error) => console.log('error connecting to db: ', error));
 };
